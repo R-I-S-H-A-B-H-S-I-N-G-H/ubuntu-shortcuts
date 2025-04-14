5fdd49e03fca69349620200389e901b1bac29301
Rishabh Singh <rishabhsingh2305@gmail.com>
2025-04-14 15:17:28 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,13 +6,12 @@ FUNCTIONS_FILE="$HOME/.my_zsh_functions"
 # URL of your GitHub repository raw function file
 REPO_URL="https://raw.githubusercontent.com/R-I-S-H-A-B-H-S-I-N-G-H/ubuntu-shortcuts/main/zsh-functions"
 
-# Download function file from GitHub to the user's home directory
-if [ ! -f "$FUNCTIONS_FILE" ]; then
-    echo "Downloading functions file to $FUNCTIONS_FILE"
-    curl -o "$FUNCTIONS_FILE" "$REPO_URL"
-else
-    echo "Functions file already exists at $FUNCTIONS_FILE"
-fi
+# Always download the latest version from GitHub
+echo "Downloading latest functions file to $FUNCTIONS_FILE"
+curl -sSf -o "$FUNCTIONS_FILE" "$REPO_URL" || {
+    echo "Failed to download the functions file from $REPO_URL"
+    exit 1
+}
 
 # Add sourcing to .zshrc if not already present
 if ! grep -q "source $FUNCTIONS_FILE" "$HOME/.zshrc"; then

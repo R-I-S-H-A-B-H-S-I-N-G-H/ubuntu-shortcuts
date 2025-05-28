53b2b12303a43f0d1dee157d66ee96ff6b557b25
Rishabh Singh <rishabhsingh2305@gmail.com>
2025-05-28 16:18:00 +0530

    Update setup.sh

--- ubuntu-setup/setup.sh
+++ ubuntu-setup/setup.sh
@@ -14,9 +14,7 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/too
 curl -s "https://get.sdkman.io" | bash
 
 # Add current user to docker group
-(sudo groupadd docker)
-(sudo usermod -aG docker $USER)
-(newgrp docker)
+sudo groupadd docker && sudo usermod -aG docker $USER && newgrp docker
 
 
 echo "✅ Setup complete. Please log out and back in (or restart terminal) for all changes to take effect."

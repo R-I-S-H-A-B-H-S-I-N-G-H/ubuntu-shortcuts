4a957a897b2d577a7adabc5451892494c0ad0019
Rishabh Singh <rishabhsingh2305@gmail.com>
2025-05-28 16:57:29 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,11 +7,11 @@ FUNCTIONS_FILE="$HOME/.my_zsh_functions"
 REPO_URL="https://raw.githubusercontent.com/R-I-S-H-A-B-H-S-I-N-G-H/ubuntu-shortcuts/main/zsh-functions"
 
 # Path for Docker Compose file
-DOCKER_COMPOSE_FILE="$HOME/docker/kafka.yml"
+DOCKER_COMPOSE_FILE="$HOME/docker/kafka/docker-compose.yml"
 DOCKER_COMPOSE_KAFKA_URL="https://raw.githubusercontent.com/R-I-S-H-A-B-H-S-I-N-G-H/ubuntu-shortcuts/refs/heads/main/ubuntu-setup/reources/kafka-setup.yml"
 
 # Ensure docker directory exists
-sudo mkdir -p "$HOME/docker"
+sudo mkdir -p "$HOME/docker/kafka"
 
 # Download the docker-compose file
 echo "Downloading Docker Compose file to $DOCKER_COMPOSE_FILE to $HOME/docker"
